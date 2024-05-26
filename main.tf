dc17b17dfb603aeda26c25efb4ef3b8d1bfba4a4
Ashok <cloud_learner01@hotmail.com>
2024-05-26 21:19:41 +0530

    Modified PIP allocation

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-resource "azurerm_resource_group" "RG_Cloud" {
+resource "azurerm_resource_group" "RG-Cloud" {
   name     = "cloud-rg"
   location = "eastus"
 }
 
 resource "azurerm_public_ip" "PIP2" {
   name                = "Cloud_PIP"
-  resource_group_name = azurerm_resource_group.RG_Cloud
-  location            = azurerm_resource_group.RG_Cloud
+  resource_group_name = azurerm_resource_group.RG-Cloud.name
+  location            = azurerm_resource_group.RG-Cloud.location
   allocation_method   = "Static"
 }
